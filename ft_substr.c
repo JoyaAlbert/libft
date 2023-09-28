f0c22cf2203e2c8e4d8bedc20a699a09594a7aac
JoyaAlbert <91436479+JoyaAlbert@users.noreply.github.com>
2023-09-28 09:59:46 +0200

    Add files via upload

--- ft_substr.c
+++ ft_substr.c
@@ -29,7 +29,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (ft_strlen(s) - start > len)
 		i = len + 1;
 	else
-		i = ft_strlen(s) - start + 2;
+		i = ft_strlen(s) - start + 1;
 	sub = (char *)malloc(i * sizeof(char));
 	if (sub == NULL)
 		return (NULL);
